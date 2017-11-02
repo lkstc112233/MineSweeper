49e2ce94adc6b21aa8e73c055c0959ee9be28408
Quantum cat <lkstc112233@users.noreply.github.com>
2017-11-02 15:59:58 -0400

    Board and cell created.

---
+++ MineSweeper/MineSweeper/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    class Board
+    {
+        int width;
+        int height;
+
+    }
+}
---
+++ MineSweeper/MineSweeper/Cell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    enum BoardCellOuterStatus
+    {
+        Unreveled,
+        Reveled,
+        Marked,
+    }
+
+    enum BoardCellInnerStatus
+    {
+        Mine,
+        Safe,
+    }
+
+    class Cell
+    {
+        int x;
+        int y;
+        private BoardCellInnerStatus m_innerStatus;
+        private BoardCellOuterStatus m_outerStatus;
+        bool isMine
+        {
+            get { return m_innerStatus == BoardCellInnerStatus.Mine; }
+        }
+    }
+}
